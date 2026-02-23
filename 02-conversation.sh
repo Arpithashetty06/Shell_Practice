a87c003f152785dccc08469f042ab2996fc613a2
Arpitha <Shettyarpithasg@gmail.com>
2026-02-23 15:03:38 +0530

    added new scripts

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Anu: Hey Manu..!" 
+echo "Manu: Hey Anu, How are you doing?" 
+echo "Anu: I am doing great Manu. What's going on?"
+echo "Manu: I started learning Shell Script Anu"
