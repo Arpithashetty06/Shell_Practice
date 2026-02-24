87a4f3a457276dcb3dbd6b4212fba5c868f068f6
Arpitha <Shettyarpithasg@gmail.com>
2026-02-24 14:20:51 +0530

    added new changes

--- 01-Helloworld.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Hello Team, How are you?"
-# "#" works only on first line remaining lines # works as command line 
-echo "Hope you all are good.!"
--- 02-conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Anu: Hey Manu..!" 
-echo "Manu: Hey Anu, How are you doing?" 
-echo "Anu: I am doing great Manu. What's going on?"
-echo "Manu: I started learning Shell Script Anu"
